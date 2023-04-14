95608d029a506663f83ed19579d85aad07466738
theebestzz <f.eminn18@gmail.com>
2023-04-14 17:08:42 +0300

    last

---
+++ python-vs/src/leibniz.nim
@@ -0,0 +1,15 @@
+import parseutils
+import strutils
+import strutils
+
+var
+    rounds = parseInt($strip(readFile("rounds.txt")))
+    x = 1.0
+    pi = 1.0
+
+for i in 2..(rounds + 2):
+    x *= -1.0
+    pi += x / float(2 * i - 1)
+
+pi *= 4.0
+echo pi.formatFloat(ffDecimal, 16)
---
+++ python-vs/src/leibniz.rs
@@ -0,0 +1,25 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let mut file = File::open("./rounds.txt").expect("file not found");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    let rounds = &contents.trim().parse::<i32>().unwrap();
+
+    // We need to use f64 instead of f32 because otherwise the precision would be off
+    let mut x: f64 = 1.0;
+    let mut pi: f64 = 1.0;
+
+    for i in 2..(rounds + 2) {
+        x *= -1.0;
+        pi = pi + (x / (2.0 * i as f64 - 1.0));
+    }
+
+    pi = pi * 4.0;
+
+    println!("{}", pi);
+}
